9b681abcce350cfbdc699eea42a36dd3e254fa4d
Dan <52573261+Syntneon@users.noreply.github.com>
2025-09-04 19:03:17 +0300

    test_2.py

--- test_main.py
+++ test_main.py
@@ -1,121 +1,41 @@
+from lesson_2 import  get_views_count, get_pairs_of_numbers, get_pct_growth, clean_name, move_zeros_2
 
-from python_lession import (is_odd, is_prime, is_arifm_progression, get_triangle_kind, is_palindrom, get_words, get_person_short_name, is_list_growing, get_pairs_number)
-
-# -------------------------Задача 1-------------------------
-
-def test_is_odd_with_even_number():
+def test_is_get_views_count():
   
-    "Тест на четное число."
-    assert is_odd(4) is True
+    assert get_views_count(1) == "1 просмотр"
+    assert get_views_count(2) == "2 просмотра"
+    assert get_views_count(5) == "5 просмотров"
+    assert get_views_count(11) == "11 просмотров"
+    assert get_views_count(21) == "21 просмотр"
+    assert get_views_count(23) == "23 просмотра"
+    assert get_views_count(114) == "114 просмотров"
 
-
-def test_is_odd_with_odd_number():
-    "Тест на нечетное число."
-    assert is_odd(5) is False
-
-
-def test_is_odd_with_zero():
-    "Тест на ноль."
-    assert is_odd(0) is True
-
-
-def test_is_odd_with_negative_even_number():
-    "Тест на отрицательное четное число."
-    assert is_odd(-10) is True
-
-
-def test_is_odd_with_negative_odd_number():
-    "Тест на отрицательное нечетное число."
-    assert is_odd(-7) is False
-
-#-------------------------Задача 2-------------------------
-
-def test_is_simple_number():
-    "Тест на простое число."
-    assert is_prime(2) is True
-
-
-def test_is_comp_number():
-    "Тест на составное число."
-    assert is_prime(4) is False
-
-
-def test_is_simple_two_digit_number():
-    "Тест на простое двузначное число."
-    assert is_prime(31) is True
-
-
-def test_is_comp_two_digit_number():
-    "Тест на составное двузначное число."
-    assert is_prime(98) is False
-
-#-------------------------Задача 3-------------------------
-
-def test_is_arifm_progression():
-    "Тест на арифметическую прогрессию."
-    assert is_arifm_progression(2, 4, 6) is True
-
-
-def test_is_not_arifm_progression():
-    "Тест на арифметическую прогрессию."
-    assert is_arifm_progression(2, 4, 8) is False
-
-#-------------------------Задача 4-------------------------
-
-def test_equilateral_triangle():
-    "Тест на равносторонний треугольник."
-    assert get_triangle_kind(2, 2, 2) == "равносторонний"
-
-
-def test_isosceles_triangle():
-    "Тест на равнобедренный треугольник."
-    assert get_triangle_kind(2, 2, 3) == "равнобедренный"
-
-
-def test_triangle():
-    "Тест на обычный треуголльник."
-    assert get_triangle_kind(2, 4, 3) == "обычный"
-
-#-------------------------Задача 5-------------------------
-
-def test_is_palindrom():
-    "Тест на палиндром."
-    assert is_palindrom("топот") is True
-
-
-def test_is_not_palindrom():
-    "Тест на палиндром."
-    assert is_palindrom("успех") is False
-
-#-------------------------Задача 6-------------------------
-
-def test_split_str():
-    "Тест на разбиение строки"
-    assert get_words("Александр Сергеевич Пушкин") == ["Александр", "Сергеевич", "Пушкин"]
-
-#-------------------------Задача 7-------------------------
-
-def test_get_person_short_name():
-    "Тест на преобразование имени и отчества в инициалы"
-    assert get_person_short_name("Лермонтов Михаил Юрьевич") == "Лермонтов М. Ю."
-
-#-------------------------Задача 8-------------------------
-
-def test_is_list_growing_true():
-    "Тест на проверку, является ли список возрастающим"
-    assert is_list_growing([1, 2, 3, 5, 7]) is True
-
-
-def test_is_list_growing_false():
-    "Тест на проверку, является ли список не возрастающим"
-    assert is_list_growing([1, 2, 5, 3, 7]) is False
-
-#-------------------------Задача 9-------------------------
 
 def test_get_pairs_with_sum():
-    "Тест на поиск пар значений которые в сумме составляют заданное число"
-    assert get_pairs_number([1, 2, 4, 3, 5, 2], 7) == [(4,3), (5,2)]
+    
+    assert get_pairs_of_numbers([1, 2, 4, 3, 5, 2], 7) == [(4,3), (5,2)]
+    assert get_pairs_of_numbers([1, 2, 4, 3, 5, 2, 1, 1], 2) == [(1, 1)]
+    assert get_pairs_of_numbers([2, 4, 5, 8], 6) ==[(4, 2)]
+    assert get_pairs_of_numbers([1, 7, 12], 7) == []
+    assert get_pairs_of_numbers([1, 7, 12, 10, 8, 7, 5], 15) == [(8, 7), (10, 5)]
 
-def test_get_pairs_with_sum_duplicates():
-    "Тест на поиск пар значений которые в сумме составляют заданное число"
-    assert get_pairs_number([1, 2, 4, 3, 5, 2, 1, 1], 2) == [(1, 1)]
+def test_get_pct_growth():
+
+    assert get_pct_growth([50, 75, 112.5, 168.75]) == [None, '50%', '50%', '50%']
+    assert get_pct_growth([200, 150, 100, 50]) == [None, '-25%', '-33%', '-50%']
+    assert get_pct_growth([100, 100, 100, 100]) == [None, None, None, None]
+    
+def test_get_clean_name():
+
+    assert clean_name("NbТим5офеев А2ндрей В88икторович") == "Тимофеев Андрей Викторович"
+    assert clean_name("") == ""
+    assert clean_name("!@#$%") == ""
+    assert clean_name("Иsвtrан Ив^%ан Ива?но)вич)")
+
+def test_get_move_zeros_2():
+    
+    assert move_zeros_2([1, 0, 0, 2, 3, 0, 1]) == [1, 2, 3, 1, 0, 0, 0]
+    assert move_zeros_2([0, 0, 0]) == [0, 0, 0]
+    assert move_zeros_2([1, 2, 3]) == [1, 2, 3]
+    assert move_zeros_2([]) == []
+    assert move_zeros_2([0, 1, 0, 2]) == [1, 2, 0, 0]
